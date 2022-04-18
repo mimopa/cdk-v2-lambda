d6ec5235a81350a523320a0ba46c3f5034c97616
mimopa <mimopapa@gmail.com>
2022-04-18 14:10:14 +0900

    Initial commit

---
+++ test/showfee_aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ShowfeeAws from '../lib/showfee_aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/showfee_aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ShowfeeAws.ShowfeeAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
